ab03d9b0beee049f1bdc97ab8e2416e694f2b732
Nizlatakar <nizlatakar@gmail.com>
2022-07-10 18:34:49 +0300

    +

---
+++ 08 Exercise Remote Data Authentication/03.Students/app.js
@@ -0,0 +1,72 @@
+let url = "http://localhost:3030/jsonstore/collections/students";
+let table = document.querySelector("#results tbody");
+let form = document.querySelector("form");
+
+window.addEventListener("load", loadStudents);
+form.addEventListener("submit", addStudents);
+
+async function addStudents(e) {
+  e.preventDefault();
+
+  let dataForm = new FormData(form);
+  let infoArr = [...dataForm.values()];
+
+  let gradeNumber = Number(infoArr[3].trim());
+  
+  table.replaceChildren();
+  try {
+    let response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: infoArr[0],
+        lastName: infoArr[1],
+        facultyNumber: infoArr[2],
+        grade: gradeNumber,
+      }),
+    });
+    if (response.ok == "false") {
+      throw new Error("Cannot create new record");
+    }
+    loadStudents();
+  } catch (error) {
+    alert(err.message)
+  }
+}
+async function loadStudents() {
+  try {
+    let response = await fetch(url);
+
+    if (response.status != 200) {
+      throw new Error("Cannot fetch data");
+    }
+    let data = await response.json();
+
+    Object.values(data).forEach((r) => {
+      let student = createElement(
+        "tr",
+        createElement("td", r.firstName),
+        createElement("td", r.lastName),
+        createElement("td", r.facultyNumber),
+        createElement("td", r.grade)
+      );
+      table.appendChild(student);
+    });
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+function createElement(type, ...content) {
+  let element = document.createElement(type);
+
+  content.forEach((c) => {
+    if (typeof c === "number" || typeof c === "string") {
+      c = document.createTextNode(c);
+    }
+    element.appendChild(c);
+  });
+  return element;
+}
