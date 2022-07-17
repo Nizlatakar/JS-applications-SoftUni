05b6a78c711be6d7762f46e686b2f58b6cbacd14
Nizlatakar <nizlatakar@gmail.com>
2022-07-17 11:36:35 +0300

    +

---
+++ 14 Exercise Client Side Rendering/01.List-Towns/app.js
@@ -0,0 +1,33 @@
+import {html, render} from '../node_modules/lit-html/lit-html.js';
+
+document.getElementById('btnLoadTowns').addEventListener('click',getTowns)
+
+const listTemplate = (data)=>html`
+<ul>
+${data.map(town =>html`<li>${town}</li>`)}
+</ul>
+`
+
+function getTowns(event){
+    event.preventDefault();
+
+  if(document.getElementById('towns').value!== ''){
+    const root = document.getElementById('root');
+    const towns = document.getElementById('towns').value.split(', ');
+
+    const ul= document.createElement('ul');
+    const result = listTemplate(towns);
+
+    render(result,root)
+
+    // towns.map(el=>{
+    //     let li =document.createElement('li');
+    //     li.textContent=el;
+    //     ul.appendChild(li);
+    // })
+    // root.appendChild(ul);
+
+    document.getElementById('towns').value ='';
+  }
+   
+}
